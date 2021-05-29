5fdffc39e86fd3d5041743306e4628718d46d99f
MoonManToken <n1gg0rk1ll3r@gmail.com>
2021-05-29 17:06:39 +0200

    blah

--- GentooInstallScript.sh
+++ GentooInstallScript.sh
@@ -1,6 +1,9 @@
 EFI="false"
 install_drive="/dev/sda"
-makeswp="true"
+makeswap="true"
+swap_size="4G"
+stage3_download="https://bouncer.gentoo.org/fetch/root/all/releases/x86/autobuilds/20210524T214502Z/stage3-i686-20210524T214502Z.tar.xz"
+
 
 if [ $EFI = "true" ]
 then
@@ -9,14 +12,44 @@ fi
 
 if [ $EFI = "false" ]
 then
+
+	#prepare file system
 	dd if=/dev/zero of=$install_drive bs=512 count=1
+	wipefs /dev/sda
 	sfdisk $install_drive << EOF
 label:dos
 ,256M
-,4G 0x82
+,$swap_size 0x82
 ;
 EOF
+	sfdisk -A $install_drive 1
+	mkfs.vfat -F 32 "$install_drive1"
+	mkfs.ext4 "$install_drive3"
+	mkswap "$install_drive2"
+	swapon "$install_drive2"
+	mount "$install_drive4" /mnt/gentoo
+
+	#set date
+	ntpd -q -g
 	
+
+	#get stage3
+	cd /mnt/gentoo
+	wget $stage3_download
+	tar xpvf "stage3*" --xattrs-include='*.*' --numeric-owner
+
+	#prepare for chroot
+	cp --dereference /etc/resolv.conf /mnt/gentoo/etc
+	mount --types proc /proc /mnt/gentoo/proc
+	mount --rbind /sys /mnt/gentoo/sys
+	mount --make-rslave /mnt/gentoo/sys
+	mount --rbind /dev /mnt/gentoo/dev
+	mount --make-rslave /mnt/gentoo/dev
+
+	#chroot
+	chroot /mnt/gentoo /bin/bash
+	source /etc/profile
+	export PS1="(chroot) $(PS1)"
 
 fi
 
